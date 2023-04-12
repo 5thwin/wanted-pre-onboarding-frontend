201683ea6d1f7be2b49255e3ec2b944f57536339
5thwin <hisy2059@naver.com>
2023-04-12 10:14:32 +0900

    FEAT: useAuth 커스텀 훅 추가

--- src/hoc/withAuthRedirect.tsx
+++ src/hoc/withAuthRedirect.tsx
@@ -1,5 +1,6 @@
 import { ComponentType, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from 'src/hooks/useAuth';
 
 export interface AuthRedirectProps {
   isAuth: boolean;
@@ -11,17 +12,17 @@ const withAuthRedirect = <P extends object>(
   const AuthRedirect: ComponentType<P & AuthRedirectProps> = (props) => {
     const navigate = useNavigate();
     const location = useLocation();
+    const { accessToken } = useAuth(); // useAuth()를 사용하여 accessToken을 가져옵니다.
 
     useEffect(() => {
-      const token = localStorage.getItem('token');
       /*토큰이 있는 상태일 때, /todo 경로로 리다이렉트 */
-      if (token) {
+      if (accessToken) {
         navigate('/todo');
       } else if (location.pathname === '/todo') {
         /*토큰이 없는 상태에서 /todo 페이지 접속 시 /signin 경로로 리다이렉트 */
         navigate('/signin');
       }
-    }, [navigate, location.pathname]);
+    }, [navigate, location.pathname, accessToken]);
 
     return props.isAuth ? <WrappedComponent {...(props as P)} /> : null;
   };
